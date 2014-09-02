684fad10bd1bf9e02da03cc2957a8567cb7197c4
nana-i <nana-i@nttr.co.jp>
2014-09-02 17:49:23 +0900

    音声認識時に「C」をランダム表示する 大きさは徐々に小さくしないといけない気がする・・・

--- app.js
+++ app.js
@@ -4,14 +4,19 @@ recognition.continuous = true;
 
 var angle = [0, 90, 180, 270];
 var size = [30, 60, 90, 120];
+var ANGLE_TOP = '上';
+var ANGLE_RIGHT = '右';
+var ANGLE_DOWN = '下';
+var ANGLE_LEFT = '左';
 
 function WebSpeechCtrl($scope) {
-	// 初期canvas表示
-	// TODO あとでランダム取得する
-	landolt(size[3], angle[1]);
-
 	// 開始ボタン押下
 	$scope.start = function() {
+
+		// 傾きを取得
+		var now_angle_index = get_random_index(angle.length);
+		landolt(size[get_random_index(size.length)], angle[now_angle_index]);
+
 		recognition.start()
 
 		//話し声の認識中
@@ -39,10 +44,41 @@ function WebSpeechCtrl($scope) {
 			var results = event.results;
 			for (var i = event.resultIndex; i<results.length; i++){
 				$scope.recognizedText = results[i][0].transcript;
+				
+				switch (angle[now_angle_index]) {
+					// 上
+					case angle[0]:
+						// TODO 音声認識($scope.recognizedText)の補正
+						if ($scope.recognizedText === ANGLE_TOP) {
+							console.log('OK');
+						}
+						break;
+					// 右
+					case angle[1]:
+						if ($scope.recognizedText === ANGLE_RIGHT) {
+							console.log('OK');
+						}
+						break;
+					// 下
+					case angle[2]:
+						if ($scope.recognizedText === ANGLE_DOWN) {
+							console.log('OK');
+						}
+						break;
+					// 左
+					case angle[3]:
+						if ($scope.recognizedText === ANGLE_LEFT) {
+							console.log('OK');
+						}
+						break;
+					default:
+						console.log('NG');
+						break;
+				}
+
 				// 再バウンド
 				$scope.$apply();
-				// TODO 判定処理を追加する
-				// landolt(size[3], angle[2]);
+				
 			}
 		};
 	};
@@ -63,9 +99,16 @@ function landolt(r, angle) {
 		var x = canvas.width/2;
 		var y = canvas.height/2;
 		var ctx = canvas.getContext('2d');
+		// キャンバスをクリアした後描画する
+		ctx.clearRect(0, 0 ,canvas.width, canvas.height);
 		ctx.beginPath();
 		ctx.arc(x, y, r, angle-Math.asin(1/5), angle+Math.asin(1/5), true);
 		ctx.arc(x, y, 3*r/5, angle+Math.asin(1/3), angle-Math.asin(1/3), false);
 		ctx.fill();
 	}
+}
+
+// 0～指定数の乱数を返す
+function get_random_index(index) {
+	return Math.floor(Math.random()*index);
 }
